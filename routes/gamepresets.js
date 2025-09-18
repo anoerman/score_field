2a9b6504ba5347fc8291d846b77efe54bce8c2ca
Noerman CZ <noerman@cognizest.com>
2025-09-18 15:19:23 +0700

    feat(game-preset): Add new game preset

--- routes/gamepresets.js
+++ routes/gamepresets.js
@@ -9,7 +9,30 @@ router.get('/', ensureAuth, async (req, res) => {
         return res.redirect('/')
     }
     const list = await GamePreset.find().sort({ createdAt: -1 })
-    res.render('gamepresets', { list })
+    res.render('game-preset/index', { list })
+})
+
+router.get('/new', ensureAuth, (req, res) => {
+    if (!req.session.user.isAdmin) {
+        return res.redirect('/')
+    }
+    res.render('game-preset/new')
+})
+
+router.post('/new', ensureAuth, async (req, res) => {
+    try {
+        console.log(req.body);
+        const inputBody = req.body;
+        await GamePreset.create({
+            ...inputBody,
+        });
+        req.session.flash = { type: 'success', message: `Preset permainan berhasil disimpan.` };
+        res.redirect('/game-presets');
+    } catch (e) {
+        console.error(e);
+        req.session.flash = { type: 'danger', message: 'Gagal menyimpan preset permainan.' };
+        res.redirect('/game-presets');
+    }
 })
 
 module.exports = router
